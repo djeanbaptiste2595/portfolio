4cefcc151c45f3e651b7673e4ca1f5c456a2f417
djeanbaptiste2595 <djeanbaptiste2595@gmail.com>
2023-02-07 18:14:00 -0500

    Want to add an additional link component for users to play the game.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
+import projImg1 from "../assets/img/carcar.png";
+import projImg2 from "../assets/img/Dimension.jpeg";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
@@ -10,13 +10,14 @@ import 'animate.css';
 export const Projects = () => {
     const projects = [
         {
-        title: "Business Startup",
-        description: "Design & Development",
+        title: "CarCar",
+        description: "User-friendly car dealership application",
         imgUrl: projImg1,
         },
         {
-        title: "Business Startup",
-        description: "Design & Development",
+        title: "Dimension & Coding",
+        description: "Fully Deployed Interactive coding Game",
+
         imgUrl: projImg2,
         },
         {
@@ -24,21 +25,7 @@ export const Projects = () => {
         description: "Design & Development",
         imgUrl: projImg3,
         },
-        {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg1,
-        },
-        {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg2,
-        },
-        {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg3,
-        },
+
     ]
     return (
         <section className="project" id="project">
